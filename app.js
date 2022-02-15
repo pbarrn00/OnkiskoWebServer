f4d7f9a748f6e125278257c4c1a614937b1cd698
NoobInTheNet <pbarrn00@estudiantes.unileon.es>
2022-02-16 00:40:47 +0100

    avances de entender handlebars

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var hbs = require('hbs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,6 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(app.get('views'), 'partials'), function (err) {});
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
