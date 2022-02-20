4c8a0c54c5b80e17cac58ee0921f514fea7b97a7
NoobInTheNet <pbarrn00@estudiantes.unileon.es>
2022-02-20 21:36:35 +0100

    actualizado vistas hbs y aprendiendo hbs templates

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Onkisko' });
+router.get('/', function (req, res, next) {
+  res.render('index',
+    {
+      page: {
+        title: 'Onkisko',
+        autor: {
+          name: 'De La',
+          surname: 'Pablo Hera'
+        }
+      }
+    });
 });
 
 module.exports = router;
